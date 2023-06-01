eafd2cb7c6d3cf190d097ec06b3f1776302e1d36
ofiddy <108938658+ofiddy@users.noreply.github.com>
2023-06-01 14:51:25 +0100

    Integrate remote database access

--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,28 @@
 const express = require('express')
 const path = require('path')
+const mysql = require('mysql')
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
+const database = process.env.DBNAME || 'challenger_db';
+
+const db = mysql.createConnection({
+  host: process.env.DBHOST || 'localhost',
+  user: process.env.DBUSER || 'dev',
+  password: process.env.DBPASS || 'dev',
+  database: process.env.DBNAME || 'challenger_db'
+});
+db.connect();
+
 // Create a GET route
 app.get('/server/express_backend', (req, res) => {
-  console.log("Intercepted right!")
-  res.send("BINGUS CONNECTED")
+  db.query(`SELECT topic FROM challenges WHERE id=1`, function (error, results, fields) {
+    if (error) throw error;
+    res.send(String(results[0].topic))
+  });
 });
 
 const root = require('path').join(__dirname, '../build')
