bb11fd8fcccdc161053bc8cf6418e85aedeed2d3
ofiddy <108938658+ofiddy@users.noreply.github.com>
2023-06-01 20:22:54 +0100

    Fix non-file encoding

--- server/server.ts
+++ server/server.ts
@@ -54,7 +54,7 @@ app.get('/uploads/*', async (req, res) => {
     s3.send(new GetObjectCommand(params)).then(async (data) => {
       res.attachment(params.Key);
       res.type(data.ContentType);
-      const bytes = await data.Body.transformToString(data.ContentEncoding);
+      const bytes = data.ContentType !== "text/plain" ? await data.Body.transformToString("binary") : await data.Body.transformToString();
 
       res.send(bytes);
     },
