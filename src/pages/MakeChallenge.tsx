7a024307004b8e18fa603162fc498bd7f88e2739
hugomiloszewski <hugomilosz@gmail.com>
2023-06-02 01:24:34 +0100

    Challenges can now be viewed from home screen

--- src/pages/MakeChallenge.tsx
+++ src/pages/MakeChallenge.tsx
@@ -1,33 +1,39 @@
 import React from 'react';
 
 const MakeChallenge = () => {
+    const handleSubmit = async (event: React.SyntheticEvent) => {
+        event.preventDefault();
 
-  const sendFiles = async () => {
-    const formData = new FormData();
-    const response = await fetch('./server/createChallenge', {
-      method: 'POST',
-      body: formData
-    });
-    const json = await response.json();
-    console.log(json);
-  }
+        const target = event.target as typeof event.target & {
+            chName: { value: string },
+            chDesc: { value: string },
+            insFile: { files: FileList }
+        };
 
-  const handleSubmit = (event: { preventDefault: () => void; }) => {
-    event.preventDefault();
-    sendFiles();
-  }
+        const formData = new FormData();
+        formData.append("name", target.chName.value);
+        formData.append("desc", target.chDesc.value);
+        formData.append("file", target.insFile.files[0], target.insFile.files[0].name);
 
-  return (
-    <div className="makeChallenge" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-      <h1>Make a Challenge</h1>
-      <form onSubmit={handleSubmit} id="form" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-        <input type="text" placeholder='Challenge Name' style={{ marginBottom: 10 }} />
-        <textarea name="Description" cols={40} rows={5} placeholder='Description' style={{ marginBottom: 10 }}></textarea>
-        <input type="file" id="myFiles" accept="image/jpg" multiple style={{ marginBottom: 10 }} />
-        <input type="submit" style={{ marginBottom: 10 }} />
-      </form>
-    </div>
-  )
+        const response = await fetch('./server/createChallenge', {
+            method: 'POST',
+            body: formData,
+        })
+    }
+
+    return (
+        <div className="makeChallenge">
+            <h1>Make a Challenge</h1>
+            <form onSubmit={handleSubmit} id="form">
+                <input type="text" placeholder='Challenge Name' name="chName" />
+                <textarea cols={40} rows={5} placeholder='Description' name="chDesc"></textarea>
+
+                {/* add initial inspiration (image/text) - this button links to UploadSubmission*/}
+                <input type="file" id="myFiles" accept="image/jpg" name="insFile" />
+                <input type="submit" />
+            </form>
+        </div>
+    )
 }
 
 export default MakeChallenge;
