61e029f822108b239ab41827d87aa6bfc50dd96c
ofiddy <108938658+ofiddy@users.noreply.github.com>
2023-06-06 13:43:57 +0100

    Fix redundant imports

--- src/pages/SearchBar.tsx
+++ src/pages/SearchBar.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate } from 'react-router-dom';
+import React from "react";
 
 export default function SearchBar() {
   const handleSubmit = async (event: React.SyntheticEvent) => {
