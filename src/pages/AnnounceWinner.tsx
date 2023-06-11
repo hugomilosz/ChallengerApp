fce9eb0289d08926d974a236e02d9e3af95e7163
hugomiloszewski <hugomilosz@gmail.com>
2023-06-11 21:09:06 +0100

    added state.id to the dependency array in AnnounceWinner

--- src/pages/AnnounceWinner.tsx
+++ src/pages/AnnounceWinner.tsx
@@ -112,7 +112,7 @@ const AnnounceWinner = () => {
             });
         };
         fetchInfo();
-    }, []);
+    }, [state.id]);
 
     const navigateToHomeScreen = () => {
         navigate('/')
