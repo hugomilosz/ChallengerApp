d358d15d9687c60379038c368e770e3071a81562
ofiddy <108938658+ofiddy@users.noreply.github.com>
2023-06-20 01:28:25 +0100

    Fix noSubmissions

--- src/pages/NoSubmissions.tsx
+++ src/pages/NoSubmissions.tsx
@@ -10,53 +10,23 @@ const NoSubmissions = () => {
 
     const navigate = useNavigate();
 
-    const [challengeInfo, setChallenge] = useState<{ name: string, description: string, imgURL: string, entryNamesUrls: Array<{ entryName: string, url: string, likeCount: number, hahaCount: number, smileCount: number, wowCount: number, sadCount: number, angryCount: number }>, deadline: Date | null, category: string }>
-        ({ name: "none", description: "none", imgURL: "", entryNamesUrls: [], deadline: null, category: "" });
+    const [challengeInfo, setChallenge] = useState<{ name: string, description: string, imgURL: string, deadline: Date | null, category: string }>
+        ({ name: "none", description: "none", imgURL: "", deadline: null, category: "" });
 
     const { state } = useLocation();
-
-    async function getReactionCount(entryWithoutPrefix: string, reactionName: string) {
-        const reactionCountResponse = await fetch(`/viewReactions/${entryWithoutPrefix}/${reactionName}`);
-        const reactionCountData = await reactionCountResponse.json();
-        switch (reactionName) {
-            case "likeCount": return reactionCountData.length > 0 ? reactionCountData[0].likeCount : 0;
-            case "hahaCount": return reactionCountData.length > 0 ? reactionCountData[0].hahaCount : 0;
-            case "smileCount": return reactionCountData.length > 0 ? reactionCountData[0].smileCount : 0;
-            case "wowCount": return reactionCountData.length > 0 ? reactionCountData[0].wowCount : 0;
-            case "sadCount": return reactionCountData.length > 0 ? reactionCountData[0].sadCount : 0;
-            case "angryCount": return reactionCountData.length > 0 ? reactionCountData[0].angryCount : 0;
-            default: console.error("not a valid reaction"); break;
-        }
-        const reactionCount = reactionCountData.length > 0 ? reactionCountData[0].likeCount : 0;
-        return reactionCount;
-    }
 
     useEffect(() => {
         const fetchInfo = async () => {
             const responseDBInfo = await fetch(`/server/challenge/${state.id}`);
             const body = await responseDBInfo.text();
             const chs = JSON.parse(body);
-            const splitArray = chs.entryNames === "" ? [] : (chs.entryNames as String).split(",")
             const deadlineDate = new Date(chs.date);
             setDeadlineDate(deadlineDate);
-            const urls = splitArray.map(async (entryName: string) => {
 
-                const url = (await (await fetch(`/uploadsURL/${entryName}`)).text());
-
-                const likeCount = await getReactionCount(entryName, "likeCount");
-                const hahaCount = await getReactionCount(entryName, "hahaCount");
-                const smileCount = await getReactionCount(entryName, "smileCount");
-                const wowCount = await getReactionCount(entryName, "wowCount");
-                const sadCount = await getReactionCount(entryName, "sadCount");
-                const angryCount = await getReactionCount(entryName, "angryCount");
-
-                return { entryName, url, likeCount, hahaCount, smileCount, wowCount, sadCount, angryCount };
-            });
             setChallenge({
                 name: chs.name as string,
                 description: chs.description as string,
                 imgURL: await (await fetch(`/uploadsURL/${chs.topic}`)).text(),
-                entryNamesUrls: await Promise.all(urls),
                 deadline: deadlineDate,
                 category: chs.subject,
             });
